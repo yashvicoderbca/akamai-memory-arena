d3a662c3621f49dbc1e4f55b9304054c07828a04
yashvicoderbca <yashvig38@gmail.com>
2026-07-05 23:13:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+//===========================================================================
 //PROJECT STATUS: DAY1 (PART1) COMPLETE
 //CURRENT STATE: CORE Structure and initization logic are done.
 //NEXT STEP: PART2 (BLOCK SPLITTING AND ALLOCATION) WILL BE ADDED TOM0RROW.
-
+//=============================================================================
 /** 
  *@file main.cpp
  *@brief custom fixed-size memory Arena(pool Allocator) for low-latency systems.
